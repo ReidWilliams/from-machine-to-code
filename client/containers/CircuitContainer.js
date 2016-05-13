c0378df32a2c7a27f8647501f3ee0abd3dd4edbe
Reid Williams <rwilliams@ideo.com>
2016-05-12 19:26:44 -0700

    more test nodes in circuit container

--- client/containers/CircuitContainer.js
+++ client/containers/CircuitContainer.js
@@ -3,7 +3,10 @@ import { connect } from 'react-redux'
 
 import SvgComponent from '../components/SvgComponent'
 import SwitchComponent from '../components/SwitchComponent'
+import AndGateComponent from '../components/AndGateComponent'
+import WireComponent from '../components/WireComponent'
 import { switchToggled } from '../reducers/circuitReducer'
+import { BOOL_OFF, BOOL_ON, BOOL_TRANSITION_OFF, BOOL_TRANSITION_ON } from '../constants/boolStates'
 
 function mapStateToProps(state) {
   return {
@@ -25,6 +28,11 @@ class CircuitContainer extends Component {
     return (
       <div className="test-svg-container centered">
         <SvgComponent>
+          <AndGateComponent boolState={BOOL_OFF} left={0} top={0}/>
+          <AndGateComponent boolState={BOOL_ON} left={100} top={0}/>
+          <SwitchComponent boolState={BOOL_OFF} left={0} top={200} />
+          <SwitchComponent boolState={BOOL_ON} left={100} top={200} />
+          <WireComponent boolState={BOOL_OFF} left={0} top={300} />
           <SwitchComponent boolState={this.props.circuitNodes[0].state} clickHandler={() => this.props.switchToggled(0)} left={0} top={200} />
         </SvgComponent>
       </div>

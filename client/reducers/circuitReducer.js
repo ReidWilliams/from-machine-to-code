0a4e3c6f1e2dd081932eb0bd25328fa12173721d
Reid Williams <rwilliams@ideo.com>
2016-06-02 18:29:57 -0700

    1-bit register works

--- client/reducers/circuitReducer.js
+++ client/reducers/circuitReducer.js
@@ -152,8 +152,9 @@ let updateNodeStateFromInputs = function(appState, nodeId, inputIndex, nodeInput
     allNodes: newAllNodes
   })
 
+  debugger
   // if node's state changed, add it to list of changed nodes
-  if (newNode.state !== appState.allNodes[nodeId]) {
+  if (newNode.state !== appState.allNodes[nodeId].state) {
     return addNodeToChangedNodes(newAppState, newNode)
   }
 

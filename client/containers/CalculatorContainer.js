f35ef09eb738e889de1009bf611dedace9a93d00
Reid Williams <rwilliams@ideo.com>
2016-11-25 21:40:40 -0800

    reverted to 3 bit

--- client/containers/CalculatorContainer.js
+++ client/containers/CalculatorContainer.js
@@ -26,26 +26,26 @@ class Calculator extends Component {
   render() { 
     // circuit nodes for input switches, output led
     let input = findObjects([0, 1, 2], this.props.circuits[CIRCUIT_NAME].allNodes)
-    let output = findObjects([128, 129, 130, 131], this.props.circuits[CIRCUIT_NAME].allNodes)
+    let output = findObjects([97, 98, 99], this.props.circuits[CIRCUIT_NAME].allNodes)
 
     return(   
       <div>
         <div className="centered body-width-line" />
         <div className="centered svg-width-circuit svg-vertical-margin">
-          <svg viewBox="0 -10 2200 1150">
-            <g transform="translate(68,720) scale(1.6)">
+          <svg viewBox="0 -10 2100 850">
+            <g transform="translate(68,590) scale(1.6)">
               <DecimalNumberComponent 
                 nodes={input} 
                 setState={this.props.setState} 
                 anchor="middle" 
                 bgX="-40" bgY="-69" bgWidth="80" bgHeight="82" />
             </g>
-            <g transform="translate(2030,740) scale(1.6)">
+            <g transform="translate(2030,590) scale(1.6)">
               <DecimalNumberComponent 
                 nodes={output} 
                 setState={undefined} 
                 anchor="middle" 
-                bgX="-40" bgY="-69" bgWidth="150" bgHeight="82" />
+                bgX="-40" bgY="-69" bgWidth="80" bgHeight="82" />
             </g>
           	<g transform="translate(140,0)">
             	<CircuitContainer circuitName="calculator" />

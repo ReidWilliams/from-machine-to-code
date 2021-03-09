ee8d7cf8ee272fede310f137b6bbae6c595f8899
Reid Williams <reid@computable.io>
2021-03-09 10:44:03 -0800

    webpack 5 dev server runs, next start replacing react w/ lit-html

--- webpack.config.js
+++ webpack.config.js
@@ -1,99 +1,38 @@
-'use strict'
+const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const webpack = require('webpack');
 
- // System Deps
-var autoprefixer =require('autoprefixer');
-var webpack =require('webpack');
-var path =require('path');
-
-
-// Only export the config to be consumed by webpack
-// It builds an instance of a webpack compiler based on this
 module.exports = {
-  // Entry points into the build process
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack-hot-middleware/client',
-    path.join(__dirname, '/client/index.js')
-  ],
-  
-  // Output build path
-  output: {
-    path: path.join(__dirname, '/dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js'
+  mode: 'development',
+  entry: {
+    main: './client/index.js',
   },
 
-  // Set up source mapping for debugging
-  devtool: "inline-source-map",
-
-  // Webpack Loaders (transformations)
-  // http://webpack.github.io/docs/using-loaders.html
+  // devtool: mode == 'development' ? 'cheap-module-eval-source-map' : 'none',
+  devServer: {
+    historyApiFallback: true,
+  },
   module: {
-    loaders: [
-      // Font files
-      {
-        test: /\.(ttf|eot|woff)(\?.*)?$/,
-        loaders: ['url']
-      },
-      // Clean up SVGs
-      {
-        test: /\.svg(\?.*)?$/,
-        loaders: ['url', 'svgo']
-      },
-      // Stylesheets
+    rules: [
       {
         test: /\.css$/,
-        loaders: ['style', 'css']
+        use: ['style-loader', 'css-loader'],
       },
-      // Compile Sass to CSS
       {
-        test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
       },
-      // ES6
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loaders: ['react-hot', 'babel']
+        test: /\.woff2$/,
+        use: ['url-loader'],
       },
-      // ESlinter for code sanity
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['eslint']
-      },
-      // Markup
-      {
-        test: /\.html$/,
-        loader: 'html'
-      },
-      // Standard JSON
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      // for bootstrap (see https://github.com/theodybrothers/webpack-bootstrap)
-      { 
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" 
-      }
-    ]
+    ],
   },
 
-  // ESLint specification
-  eslint: {
-    parser: 'babel-eslint'
-  },
-
-  // Handle all vendor prefixes
-  postcss: () => {
-    return [autoprefixer];
-  },
-
-  // http://webpack.github.io/docs/using-plugins.html  
-  plugins: [
-    // https://github.com/glenjamin/webpack-hot-middleware 
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  }
 };
